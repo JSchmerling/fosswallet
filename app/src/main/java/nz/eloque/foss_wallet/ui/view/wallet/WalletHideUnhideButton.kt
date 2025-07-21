01c120bfc0b0fb3271796f0ebcd4ebb550f4d8f7
JSchmerling <mail@schmerl.ing>
2025-07-21 16:19:26 +0200

    Update WalletHideUnhideButton.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/view/wallet/WalletHideUnhideButton.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/view/wallet/WalletHideUnhideButton.kt
@@ -1,1 +1,23 @@
+package nz.eloque.foss_wallet.ui.view.pass
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import nz.eloque.foss_wallet.R
+
+@Composable
+fun WalletHideUnhideButton(
+    isAuthenticated: Boolean,
+    onClick: () -> Unit
+) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = if (isAuthenticated) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
+            contentDescription = if (isAuthenticated) stringResource(R.string.unhide) else stringResource(R.string.hide)
+        )
+    }
+}
