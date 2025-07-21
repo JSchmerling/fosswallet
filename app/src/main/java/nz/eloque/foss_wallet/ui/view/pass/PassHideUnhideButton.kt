ad5340cb4b25456e9712d1868ba6fc90f299ea46
JSchmerling <mail@schmerl.ing>
2025-07-21 16:02:33 +0200

    Update PassHideUnhideButton.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/view/pass/PassHideUnhideButton.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/view/pass/PassHideUnhideButton.kt
@@ -1,1 +1,10 @@
+package nz.eloque.foss_wallet.ui.view.pass
 
+import nz.eloque.foss_wallet.R
+
+@Composable
+fun PassHideUnhideButton(
+    IconButton(onClick = {}) {
+        Icon(imageVector = Icons.Default.Share, contentDescription = stringResource(R.string.share))
+    }
+}
