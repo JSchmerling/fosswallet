659f6abffb78ae8b543ee4efbafcc044d3601bba
JSchmerling <mail@schmerl.ing>
2025-08-09 14:14:47 +0200

    Update LocationButton.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/LocationButton.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/LocationButton.kt
@@ -4,7 +4,6 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.location.Location
 import android.util.Log
-import androidx.core.net.toUri
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Icon
@@ -12,6 +11,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.net.toUri
 
 
 @Composable
