3f244b4110da9526498578a6b4e9134108f8c4e2
JSchmerling <mail@schmerl.ing>
2025-08-25 12:32:55 +0200

    Update Color.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/theme/Color.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package nz.eloque.foss_wallet.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +11,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val ColorScheme.deleteRed: Color
+@Composable get() = if (isSystemInDarkTheme()) Color(0xFFFF453A) else Color(0xFFFF3B30)
