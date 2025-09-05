1397a45fa0ef780127000891afbad1ae5451d537
JSchmerling <mail@schmerl.ing>
2025-09-05 13:09:34 +0200

    Update SelectionActions.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/SelectionActions.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/SelectionActions.kt
@@ -40,7 +40,7 @@ fun SelectionActions(
         horizontalAlignment = Alignment.End
     ) {
         FloatingActionButton(
-            containerColor = MaterialTheme.colorScheme.error,
+            containerColor = MaterialTheme.colorScheme.delete,
             onClick = {
                 coroutineScope.launch(Dispatchers.IO) {
                     selectedPasses.forEach { passViewModel.delete(it) }
@@ -55,6 +55,13 @@ fun SelectionActions(
                 coroutineScope.launch(Dispatchers.IO) {
                     selectedPasses.forEach { passViewModel.archive(it) }
                     selectedPasses.clear()
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.deleted),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
             },
         ) {
