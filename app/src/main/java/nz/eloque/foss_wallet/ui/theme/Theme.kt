a524815a1c4d912a3606f9f2c473ddc331c957db
JSchmerling <mail@schmerl.ing>
2025-09-05 22:55:28 +0200

    Update Theme.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/theme/Theme.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/theme/Theme.kt
@@ -18,15 +18,17 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    delete = DeleteRedLight
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    delete = DeleteRedDark
 
-    /* Other default colors to override
+/* Other default colors to override
 background = Color(0xFFFFFBFE),
 surface = Color(0xFFFFFBFE),
 onPrimary = Color.White,
