4cae91698a3263b61428a6006c8397d28e71a7c5
JSchmerling <mail@schmerl.ing>
2025-09-16 01:14:41 +0200

    Update WalletScaffold.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/WalletScaffold.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/WalletScaffold.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
