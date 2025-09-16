67645c7e9832d7788595a13cbf82ed599f9ac2a9
JSchmerling <mail@schmerl.ing>
2025-09-16 21:02:47 +0200

    Update WalletScaffold.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/WalletScaffold.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/WalletScaffold.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material.icons.Icons
@@ -72,7 +70,9 @@ fun WalletScaffold(
         floatingActionButton = {
             Box(
                 modifier = Modifier.padding(
-                    bottom = navigationBarsPadding().imePadding()
+                    bottom = WindowInsets.navigationBars
+                        .asPaddingValues()
+                        .calculateBottomPadding()
                 )
             ) {
                 floatingActionButton()
