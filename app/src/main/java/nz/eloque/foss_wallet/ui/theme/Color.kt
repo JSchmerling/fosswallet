1155d6b4de3e7c3798d92d4f634589da2b15e0f9
JSchmerling <mail@schmerl.ing>
2026-02-28 07:53:07 +0100

    Update Color.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/theme/Color.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/theme/Color.kt
@@ -9,6 +9,3 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650A4)
 val PurpleGrey40 = Color(0xFF625B71)
 val Pink40 = Color(0xFF7D5260)
-
-val Grey500 = Color(0xFF9E9E9E)
-val Orange300 = Color(0xFFFFB74D)
