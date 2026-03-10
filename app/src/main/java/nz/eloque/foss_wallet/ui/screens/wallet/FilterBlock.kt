871df884a87c406a11bb89cb326496b05e500329
JSchmerling <mail@schmerl.ing>
2026-03-10 16:45:07 +0100

    Update FilterBlock.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/FilterBlock.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/FilterBlock.kt
@@ -7,35 +7,26 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Sort
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalResources
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import nz.eloque.foss_wallet.R
 import nz.eloque.foss_wallet.model.PassType
 import nz.eloque.foss_wallet.model.SortOption
 import nz.eloque.foss_wallet.model.Tag
 import nz.eloque.foss_wallet.ui.components.ChipSelector
-import nz.eloque.foss_wallet.ui.components.FilterBar
-import nz.eloque.foss_wallet.ui.components.SelectionMenu
 import nz.eloque.foss_wallet.ui.components.tag.TagRow
 
 @Composable
@@ -50,32 +41,44 @@ fun FilterBlock(
     val resources = LocalResources.current
 
     var lastScrollIndex by remember { mutableIntStateOf(0) }
+    var filtersShown by remember { mutableStateOf(true) }
 
     LaunchedEffect(listState) {
         snapshotFlow { listState.firstVisibleItemIndex }
-            .collect { currentIndex -> lastScrollIndex = currentIndex }
+            .collect { currentIndex ->
+                filtersShown = currentIndex <= lastScrollIndex
+                lastScrollIndex = currentIndex
+            }
     }
-    
-    Row(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxWidth()
+
+    AnimatedVisibility(
+        visible = filtersShown,
+        enter = expandVertically(
+            animationSpec = tween(durationMillis = 300)) + fadeIn(animationSpec = tween(300)),
+        exit = shrinkVertically(
+            animationSpec = tween(durationMillis = 300)) + fadeOut(animationSpec = tween(300))
     ) {
-        ChipSelector(
-            options = PassType.all(),
-            selectedOptions = passTypesToShow,
-            onOptionSelected = { passTypesToShow.add(it) },
-            onOptionDeselected = { passTypesToShow.remove(it) },
-            optionLabel = { resources.getString(it.label) },
+        Row(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxWidth()
-        )
-        TagRow(
-            tags = tags,
-            selectedTag = tagToFilterFor.value,
-            onTagSelected = { tagToFilterFor.value = it },
-            onTagDeselected = { tagToFilterFor.value = null },
-            walletViewModel = walletViewModel,
-            modifier = Modifier.fillMaxWidth()
-        )
+        ) {
+            ChipSelector(
+                options = PassType.all(),
+                selectedOptions = passTypesToShow,
+                onOptionSelected = { passTypesToShow.add(it) },
+                onOptionDeselected = { passTypesToShow.remove(it) },
+                optionLabel = { resources.getString(it.label) },
+                modifier = Modifier.fillMaxWidth()
+            )
+            TagRow(
+                tags = tags,
+                selectedTag = tagToFilterFor.value,
+                onTagSelected = { tagToFilterFor.value = it },
+                onTagDeselected = { tagToFilterFor.value = null },
+                walletViewModel = walletViewModel,
+                modifier = Modifier.fillMaxWidth()
+           )
+        }
     }
 }
