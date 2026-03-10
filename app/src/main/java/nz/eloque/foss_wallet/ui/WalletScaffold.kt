8575bd0dd82ed555b50e40a635fb30a604da152b
JSchmerling <mail@schmerl.ing>
2026-03-10 13:29:37 +0100

    Update WalletScaffold.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/WalletScaffold.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/WalletScaffold.kt
@@ -1,12 +1,19 @@
 package nz.eloque.foss_wallet.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,18 +23,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import nz.eloque.foss_wallet.R
 import nz.eloque.foss_wallet.ui.components.NavigationRow
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -59,7 +70,11 @@ fun WalletScaffold(
                         }
                     } else {
                         Box {
-                            Image(painter = painterResource(id = R.drawable.icon), contentDescription = null)
+                            Image(
+                                painter = painterResource(id = R.drawable.icon),
+                                contentDescription = null,
+                                modifier = Modifier.fillMaxSize()
+                            )
                         } 
                     }
                 },
@@ -68,7 +83,17 @@ fun WalletScaffold(
             )
         },
         contentWindowInsets = WindowInsets.statusBars,
-        bottomBar = if (showNavigation) NavigationRow(navController: NavController),
+        bottomBar = {
+            if (showNavigation) {
+                AnimatedVisibility(
+                    visible = showNavBar,
+                    enter = slideInVertically(initialOffsetY = { it }),
+                    exit = slideOutVertically(targetOffsetY = { it })
+                ) {
+                    NavigationRow(navController = navController)
+                }
+            }
+        },
         floatingActionButton = {
             Box(
                 modifier = Modifier.padding(
@@ -85,7 +110,7 @@ fun WalletScaffold(
             .padding(innerPadding)
             .padding(horizontal = 8.dp)
         ) {
-            content(scrollBehavior)
+            content.invoke(scrollBehavior)
         }
     }
 }
