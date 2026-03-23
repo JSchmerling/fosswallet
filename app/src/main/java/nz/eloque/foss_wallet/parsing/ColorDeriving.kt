f0e219c3b2ee0706634e09f15ff9e7993b4452e3
JSchmerling <mail@schmerl.ing>
2026-03-23 19:41:11 +0100

    Update ColorDeriving.kt

--- app/src/main/java/nz/eloque/foss_wallet/parsing/ColorDeriving.kt
+++ app/src/main/java/nz/eloque/foss_wallet/parsing/ColorDeriving.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.core.graphics.ColorUtils
 import androidx.core.graphics.get
 import nz.eloque.foss_wallet.model.PassColors
+import nz.eloque.foss_wallet.utils.ExtensionFunctions
 
 fun Bitmap?.derivePassColors(): PassColors? {
     if (this == null) return null
@@ -41,5 +42,3 @@ private fun Color.clamp(): Color {
         green = this.green.round()
     )
 }
-
-private fun Float.round(): Float = (this * 10).toInt() / 10f
