e426b285cecfa2f2b526e3d5d35558102d08581e
JSchmerling <mail@schmerl.ing>
2026-03-24 14:21:01 +0100

    Update WalletScreen.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/WalletScreen.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/WalletScreen.kt
@@ -43,6 +43,7 @@ import nz.eloque.foss_wallet.ui.Screen
 import nz.eloque.foss_wallet.ui.WalletScaffold
 import nz.eloque.foss_wallet.ui.components.FabMenu
 import nz.eloque.foss_wallet.ui.components.FabMenuItem
+import nz.eloque.foss_wallet.ui.components.NavigationRow
 import nz.eloque.foss_wallet.ui.components.SelectionMenu
 import nz.eloque.foss_wallet.utils.PkpassMimeTypes
 
