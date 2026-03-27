2148eeef4b1dc46f4abde29c8e3d8bfffa34d2f0
JSchmerling <mail@schmerl.ing>
2026-03-27 19:00:27 +0100

    Update WalletScaffold.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/WalletScaffold.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/WalletScaffold.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -68,7 +69,7 @@ fun WalletScaffold(
                             Image(
                                 painter = painterResource(id = R.drawable.icon),
                                 contentDescription = null,
-                                modifier = Modifier.size(24.dp)
+                                modifier = Modifier.fillMaxSize()
                             )
                         }
                     }
