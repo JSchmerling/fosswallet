e7c355b4e1c927ef9fa6a03bde03de9f95d462d4
JSchmerling <mail@schmerl.ing>
2026-03-27 22:59:30 +0100

    Update FilterBar.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/FilterBar.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/FilterBar.kt
@@ -13,12 +13,11 @@ import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,8 +29,8 @@ fun FilterBar(
     onSearch: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val focusManager = LocalFocusManager.current
     var query by rememberSaveable { mutableStateOf("") }
-    
     SearchBar(
         windowInsets = WindowInsets(0.dp),
         inputField = {
