a47135a2c1c8a5a4ff5c22b73e021166cbbd0910
JSchmerling <mail@schmerl.ing>
2026-03-27 16:54:11 +0100

    Update NavigationRow.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/NavigationRow.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/NavigationRow.kt
@@ -28,6 +28,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
@@ -97,7 +99,7 @@ fun NavigationRow(
                 onClick = {
                     selectedTabIndex = 0
                     imeVisible = true
-                    keyboardController?.hide()
+                    keyboardController?.show()
                 },
                 icon = { Icon(Icons.Default.Search, contentDescription = stringResource(R.string.search)) },
                 label = { Text(stringResource(R.string.search)) }
