db968b0f8b6dedd263ce2e627b995501d984bc22
JSchmerling <mail@schmerl.ing>
2026-05-10 13:05:21 +0200

    Clean up FilterBlock by removing unused code
    Removed unused imports and parameters from FilterBlock.

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/FilterBlock.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/FilterBlock.kt
@@ -1,38 +1,22 @@
 package nz.eloque.foss_wallet.ui.screens.wallet
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Sort
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import nz.eloque.foss_wallet.R
 import nz.eloque.foss_wallet.model.PassType
 import nz.eloque.foss_wallet.model.SortOption
 import nz.eloque.foss_wallet.model.Tag
-import nz.eloque.foss_wallet.ui.components.FilterBar
 import nz.eloque.foss_wallet.ui.components.SelectionMenu
 import nz.eloque.foss_wallet.ui.components.tag.TagRow
 
 @Composable
 fun FilterBlock(
     walletViewModel: WalletViewModel,
-    onSortChange: (SortOption) -> Unit,
     tags: Set<Tag>,
 ) {
     val resources = LocalResources.current
