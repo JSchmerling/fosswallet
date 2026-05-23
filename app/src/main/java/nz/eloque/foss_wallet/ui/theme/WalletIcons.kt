9924f95bb977fd24fd265fd0321a1528a0030a47
JSchmerling <mail@schmerl.ing>
2026-05-23 20:13:22 +0200

    Add WalletIcons object with SearchArchive icon

---
+++ app/src/main/java/nz/eloque/foss_wallet/ui/theme/WalletIcons.kt
@@ -0,0 +1,32 @@
+package nz.eloque.foss_wallet.ui.theme
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.addPathNodes
+import androidx.compose.ui.unit.dp
+
+object WalletIcons {
+    val SearchArchive: ImageVector = ImageVector.Builder(
+        defaultWidth = 24.dp,
+        defaultHeight = 24.dp,
+        viewportWidth = 24f,
+        viewportHeight = 24f
+    ).apply {
+        addPath(
+            pathData = addPathNodes("M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"),
+            fill = SolidColor(Color.Unspecified)
+        )
+        addGroup(
+            translationX = 6.86f,
+            translationY = 6.86f,
+            scaleX = 0.22f,
+            scaleY = 0.22f
+        ) {
+            addPath(
+                pathData = addPathNodes("M20.54 5.23l-1.39-1.68C18.88 3.21 18.47 3 18 3H6c-.47 0-.88.21-1.16.55L3.46 5.23C3.17 5.57 3 6.02 3 6.5V19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6.5c0-.48-.17-.93-.46-1.27zM12 17.5L6.5 12H10v-2h4v2h3.5L12 17.5zM5.12 5l.81-1h12l.94 1H5.12z"),
+                fill = SolidColor(Color.Unspecified)
+            )
+        }
+    }.build()
+}
